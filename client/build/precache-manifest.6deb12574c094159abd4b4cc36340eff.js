a21409d42ea5080ef75912ff60642fe7b1981c7f
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-05 13:50:39 -0600

    commiting files from Redux repo

---
+++ client/build/precache-manifest.6deb12574c094159abd4b4cc36340eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b2b4c3c6c6f91c110d99054457f28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb18695ccfaec8450930",
+    "url": "/static/css/main.581df338.chunk.css"
+  },
+  {
+    "revision": "4f26fde36283db9ac385",
+    "url": "/static/js/2.1edd8bd5.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.1edd8bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb18695ccfaec8450930",
+    "url": "/static/js/main.28a615f2.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
